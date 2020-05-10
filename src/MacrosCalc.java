49ac91cc05e0f1dcc6417067256077f0794949e3
nickbui <nicholas.t.bui@gmail.com>
2020-05-10 14:17:37 -0400

    Third commit

--- src/MacrosCalc.java
+++ src/MacrosCalc.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class MacrosCalc {
 
+	private static float finalCalories;
+
 	public static void main(String[] args) {
 	
 		Scanner scan = new Scanner(System.in);
@@ -29,9 +31,6 @@ public class MacrosCalc {
 		System.out.println("Enter your Body Fat Percentage: ");
 		float bodyFatPercentage = scan.nextFloat() / 100;
 		
-		
-		
-		
 		float BMR = (float) ((10 * weight) + (6.25 * height) - (5 * age) + 5);
 		float maintenance = BMR * lifeStyleMultiplier;
 		float weightToPounds = (float) (weight / 0.453592);
@@ -40,6 +39,22 @@ public class MacrosCalc {
 		System.out.println("BMR is: " + BMR);
 		System.out.println("Maintenance Calories is: " + maintenance);
 		System.out.println("Lean Body Mass is " + leanBodyMass);
+		
+		System.out.println("Do you want to 1.Cut, 2.Bulk, or 3.Maintain? Please enter a number");
+		int goal = scan.nextInt();
+		while (goal < 1 || goal > 3) {
+			System.out.println("Please enter a number within the Given Parameters: ");
+			goal = scan.nextInt();
+		}
+		if (goal == 1) {
+			finalCalories = maintenance - (maintenance * .10f);
+		} else if (goal == 2) {
+			finalCalories = maintenance + (maintenance * .10f);
+		} else {
+			finalCalories = maintenance;
+		}
+		
+		System.out.println(finalCalories);
 		
 	}
 
